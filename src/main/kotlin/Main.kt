3a428d7d9799c9c81d6e1ea6338f1be52a09696b
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-08-07 16:33:13 -0400

    Creacion de la clase generica

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,12 @@
 fun main(args: Array<String>) {
     println("Hello World!")
-    println("Program arguments: ${args.joinToString()}")
+}
+
+data class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: Difficulty
+)
+enum class Difficulty {
+    EASY, MEDIUM, HARD
 }
